1853b87c616270e534e93956f3daaabfa5e14c59
Osemudiamen Elebesunu <eronscode@gmail.com>
2023-12-11 21:27:45 +0100

    refactor: modifies look and feel of animal details page

--- src/pages/AnimalDetails.tsx
+++ src/pages/AnimalDetails.tsx
@@ -4,6 +4,7 @@ import Rate from 'rc-rate';
 import 'rc-rate/assets/index.css';
 import { Hero } from '@/components/ui/Hero';
 import MapPinSVG from '@/assets/map-pin.svg?react';
+import StarSVG from '@/assets/star.svg?react';
 import ThumbsUpSVG from '@/assets/thumbs-up.svg?react';
 import ThumbsDownSVG from '@/assets/thumbs-down.svg?react';
 import HeartSVG from '@/assets/heart.svg?react';
@@ -11,13 +12,13 @@ import { Button } from '@/components/ui/Button';
 import { Routes } from '@/config/routes';
 import { useSearchAnimals } from '@/hooks/useSearchAnimals';
 import { Preferences } from '@/enums';
-import { slugUtils } from '@/utils/slug';
+import { stringUtils } from '@/utils/formatString';
 
 export const AnimalDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
 
-  const { decodeSlug } = slugUtils;
+  const { decodeSlug, convertToTitleCase } = stringUtils;
   const name = decodeSlug(String(params?.name) ?? '');
 
   const { searchQuery, toggleFavourites, handleRating, handlePreference } = useSearchAnimals(name);
@@ -32,6 +33,8 @@ export const AnimalDetails = () => {
   if (!animalDetail) {
     return null;
   }
+
+  console.log({ animalDetail });
 
   return (
     <>
@@ -48,15 +51,31 @@ export const AnimalDetails = () => {
         }
       />
       <div className="container mx-auto py-10">
-        <h1 className="text-3xl font-semibold mb-6">{animalDetail.name}</h1>
+        <div className="mb-4">
+          <h1 className="text-3xl font-semibold mb-2">{animalDetail.name}</h1>
+          <div className="flex items-center gap-3">
+            <div className="flex gap-2 items-center">
+              <MapPinSVG className="w-4" />
+              {animalDetail.locations.map((value) => value).join(', ')}
+            </div>
+            <Rate
+              key={animalDetail?.rating}
+              defaultValue={animalDetail?.rating ?? 0}
+              character="★"
+              disabled
+            />
+            <span className="text-gray-500">({animalDetail?.rating ?? 0} / 5)</span>
+          </div>
+        </div>
+
         <div className="mb-8">
           <Button
             label={
               <span className="flex items-center gap-2">
-                {animalDetail.isFavourite ? 'Remove from favourite' : 'Add to favourite'}
                 <HeartSVG
                   className={classNames('w-5', { 'fill-red-600 ': animalDetail.isFavourite })}
                 />
+                {animalDetail.isFavourite ? 'Remove from favourite' : 'Add to favourite'}
               </span>
             }
             variant={animalDetail.isFavourite ? 'primary' : 'secondary'}
@@ -64,54 +83,65 @@ export const AnimalDetails = () => {
           />
         </div>
         <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Rating</h2>
+          <h2 className="text-xl font-semibold mb-2">Leave a Rating!</h2>
           <Rate
             onChange={(value) => handleRating(value)}
             allowHalf
             defaultValue={animalDetail?.rating ?? 0}
-            character="★"
+            character={() => (
+              <div
+                className={classNames(
+                  '[&>svg]:fill-current [&>svg]:stroke-black [&>svg]:stroke-1 w-12 h-12',
+                )}
+              >
+                <StarSVG />
+              </div>
+            )}
           />
           <div className="flex gap-2 items-center"></div>
-        </div>
-        <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Location</h2>
-          <div className="flex gap-2 items-center">
-            <MapPinSVG className="w-4" />
-            {animalDetail.locations.map((value) => value).join(', ')}
-          </div>
         </div>
         <div className="mb-8">
           <h2 className="text-xl font-semibold mb-2">Taxonomy</h2>
           <ul className="list-none">
             {Object.entries(animalDetail.taxonomy).map(([key, value]) => (
               <li key={key}>
-                <span className="font-semibold">{key}</span>: <span>{value}</span>
+                <span className="font-semibold">{convertToTitleCase(key)}</span>:{' '}
+                <span>{value}</span>
               </li>
             ))}
           </ul>
         </div>
         <div>
           <h2 className="text-xl font-semibold mb-2">Characteristics</h2>
-          <ul className="flex flex-col gap-4">
+          <ul className="sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-y-8 gap-x-5 3xl:gap-y-10 ">
             {Object.entries(animalDetail.characteristics).map(([key, value]) => (
-              <li className="flex gap-1" key={key}>
+              <li
+                className="flex flex-col justify-between gap-1 min-h-[230px] border border-gray-300 p-4 rounded-md"
+                key={key}
+              >
                 <div>
-                  <span className="font-semibold">{key}</span>:{' '}
+                  <p className="font-semibold">{convertToTitleCase(key)}</p>
                   <span>
                     <>{value}</>
                   </span>
                 </div>
-                <span className="flex gap-3">
-                  <button onClick={() => handlePreference(Preferences.LIKE, animalDetail, key)}>
+                <span className="w-full flex gap-3">
+                  <button
+                    className="w-full flex justify-center bg-gray-100 rounded py-3"
+                    onClick={() => handlePreference(Preferences.LIKE, animalDetail, key)}
+                  >
                     <ThumbsUpSVG
-                      className={classNames('w-5', {
+                      className={classNames('w-7', {
                         'fill-secondary': animalDetail?.preferences?.[key] === Preferences.LIKE,
                       })}
                     />
                   </button>
-                  <button onClick={() => handlePreference(Preferences.DISLIKE, animalDetail, key)}>
+                  <button
+                    className="w-full flex justify-center bg-gray-100 rounded py-3"
+                    onClick={() => handlePreference(Preferences.DISLIKE, animalDetail, key)}
+                  >
                     <ThumbsDownSVG
-                      className={classNames('w-5', {
+                      className={classNames('w-7', {
                         'fill-secondary': animalDetail?.preferences?.[key] === Preferences.DISLIKE,
                       })}
                     />
