af4e1dd2b4774bcf17d05d2076326f89b3da648f
Osemudiamen Elebesunu <eronscode@gmail.com>
2023-12-11 20:45:55 +0100

    feat: defines persistent query options in app

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/global.css';
+import { QueryClient } from '@tanstack/react-query';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import {
+  PersistQueryClientProvider,
+  persistQueryClient,
+} from '@tanstack/react-query-persist-client';
 
-const EntryPoint = () => <App />;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0,
+      gcTime: Infinity,
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister,
+});
+
+export const EntryPoint = () => (
+  <PersistQueryClientProvider persistOptions={{ persister }} client={queryClient}>
+    <App />
+  </PersistQueryClientProvider>
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
