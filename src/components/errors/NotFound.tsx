ebb2cbcd3f6addbcfbeb50ad31a13056d2ec474e
Osemudiamen Elebesunu <eronscode@gmail.com>
2023-12-11 20:48:49 +0100

    feat: implements notfound component

---
+++ src/components/errors/NotFound.tsx
@@ -0,0 +1,9 @@
+export const NotFound = () => {
+  return (
+    <div className="text-center w-full mt-8">
+      <div></div>
+      <h1 className="font-bold text-5xl mt-8 mb-4">404</h1>
+      <h2 className="font-medium text-2xl">Opps! Page Not Found</h2>
+    </div>
+  );
+};
