e1861cec187a64f2d406f9826e7d6f048d7240d3
Osemudiamen Elebesunu <eronscode@gmail.com>
2023-12-11 20:46:40 +0100

    feat: defines and implements all app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home } from '@/pages/Home';
+import { AnimalDetails } from './pages/AnimalDetails';
+import { Layout } from './components/layout/Layout';
+import { Routes } from './config/routes';
+import { Favourites } from './pages/Favourites';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: Routes.public.animalDetails(':name'),
+        element: <AnimalDetails />,
+      },
+      {
+        path: Routes.public.favourites,
+        element: <Favourites />,
+      },
+    ],
+    // errorElement: <NotFound />,
+  },
+]);
+
 function App() {
-  return <p>Hello</p>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
